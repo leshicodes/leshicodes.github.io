0f7d5bdbbe6df0f041f36cb14efdada0213e5cc4
James Ambrose <galaxy2198@gmail.com>
2025-10-30 20:44:19 -0500

    Refactor resource pack metadata script to dynamically retrieve and hash zip files

--- scripts/Create-ResourcePackMetadata.ps1
+++ scripts/Create-ResourcePackMetadata.ps1
@@ -1,4 +1,10 @@
-$FileHash = $(Get-FileHash -Path "docs\Minecraft\static\nutville-data-pack-server.zip" -Algorithm SHA1)
+$getZipFiles = $(Get-ChildItem -Path "docs\Minecraft\static" -Filter "*.zip"  -Recurse) | Where-Object { $_.Name -like "nutville-data-pack-*" }
+
+foreach ($file in $getZipFiles) {
+    $FileHash = $(Get-FileHash -Path $file.FullName -Algorithm SHA1)
+}
+
+#$FileHash = $(Get-FileHash -Path "docs\Minecraft\static\nutville-data-pack-server.zip" -Algorithm SHA1)
 #$FileHash
 
 $guid = New-Guid
